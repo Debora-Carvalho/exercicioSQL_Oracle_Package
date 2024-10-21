755f9698a74eb2d281c4336727bb9959f452b8dc
Debora <deboravolc2@gmail.com>
2024-10-21 11:11:20 -0300

    adicionar estrutura function do bonus por departamento

--- func_calculoSalario.sql
+++ func_calculoSalario.sql
@@ -1,4 +1,4 @@
--- Função para o Cálculo do INSS
+-- FUNÇÃO PARA CÁLCULO DO INSS
 -- INSS será igual a 20% do SB
 
 CREATE OR REPLACE FUNCTION FUNC_CALC_INSS
@@ -16,7 +16,7 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('INSS: ' || FUNC_CALC_INSS(7000));
 END;
 
--- Função para o Cálculo do FGTS
+-- FUNÇÃO PARA CÁLCULO DO FGTS
 -- FGTS será igual a 8% do SB.
 
 CREATE OR REPLACE FUNCTION FUNC_CALC_FGTS 
@@ -34,7 +34,7 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('FGTS: ' || FUNC_CALC_FGTS(7000));
 END;
 
--- Função para o Cálculo do Convênio Médico
+-- FUNÇÃO PARA CÁLCULO DO CONVÊNIO MÉDICO
 -- SB < 2000, então CM terá valor de 200.00
 -- SB >= 2000 e < 5000, então CM terá valor de 350.00
 -- SB >= 5000, então CM terá valor de 500.00
@@ -61,4 +61,28 @@ BEGIN
 END;
 
 
--- Função para o Cálculo do Bônus por Departamento
+-- FUNÇÃO PARA O CÁLCULO DE BÔNUS POR DEPARTAMENTO
+-- Presidência - 500% do SB
+-- Diretoria -  300% do SB
+-- Vendas - 100% do SB
+-- TI - 200% do SB
+
+CREATE OR REPLACE FUNCTION FUNC_CALC_BONUSDEP
+(
+    V_SB IN NUMBER,
+    V_DEPARTAMENTO IN NUMBER
+)
+RETURN NUMBER
+IS
+BEGIN
+    IF V_DEPARTAMENTO = 1 THEN
+        RETURN V_SB * 5;
+    ELSE IF V_TIME = 2 THEN
+            RETURN V_SB * 3;
+        ELSE IF V_TIME = 4 THEN
+            RETURN V_SB;
+            ELSE RETURN V_SB * 2;
+            END IF;
+        END IF;
+    END IF;
+END;
